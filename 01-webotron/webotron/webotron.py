885f9484639ec829d1f6d954e3de6fc0f8cabbb2
Todd Wade <twade@paywtih.com>
2020-04-20 11:00:57 -0700

    Sync Directory with S3_2

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -1,5 +1,7 @@
 import boto3
 import click
+from pathlib import Path
+import mimetypes
 
 session = boto3.Session(profile_name='pythonAutomation')
 s3 = session.resource('s3')
@@ -21,6 +23,70 @@ def list_bucket_objects(bucket):
     'List objects in an s3 bucket'
     for obj in s3.Bucket(bucket).objects.all():
         print (obj)
+
+@cli.command('setup-bucket')
+@click.argument('bucket')
+def setup_bucket(bucket):
+    "Create and configure an s3 bucket"
+    s3_bucket = s3.create_bucket(
+        Bucket='bucket',
+        CreateBucketConfiguration={'LocationConstraint':session.region_name})
+
+    policy = '''
+    {
+      "Version":"2012-10-17",
+      "Statement":[{
+      "Sid":"PublicReadGetObject",
+      "Effect":"Allow",
+      "Principal": "*",
+          "Action":["s3:GetObject"],
+          "Resource":["arn:aws:s3:::%s/*"
+          ]
+        }
+      ]
+    }
+    ''' % s3_bucket.name
+    policy = policy.strip()
+
+    pol = s3_bucket.Policy()
+    pol.put(Policy=policy)
+
+    ws = new_bucket.Website()
+    ws.put(WebsiteConfiguration={
+        'ErrorDocument': {
+            'Key': 'error.html'
+        },
+        'IndexDocument': {
+            'Suffix': 'index.html'
+        }
+    })
+
+    return
+def upload_file(s3_bucket, path, key):
+    content_type = mimetypes.guess_type(key)[0] or 'text/plain'
+    s3_bucket.upload_file(
+        path,
+        key,
+        ExtraArgs={
+            'ContentType': 'text/html'
+        })
+
+@cli.command('sync')
+@click.argument('pathname', type=click.Path(exists=True))
+@click.argument('bucket')
+def sync(pathname, bucket):
+    "Sync contents of PATHNAME to BUCKET"
+    s3_bucket = s3.Bucket(bucket)
+    root = Path(pathname).expanduser().resolve()
+
+    def handle_directory(target):
+        for p in target.iterdir():
+            if p.is_dir(): handle_directory(p)
+            if p.is_file(): upload_file(s3_bucket, str(p), str(p.relative_to(root)))
+
+
+
+    handle_directory(root)
 
 if __name__ == '__main__':
     cli()
